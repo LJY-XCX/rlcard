063c7fb7e5d6d044afca9be1f987f95e0cf999be
LittleHelicopter <yahan.zheng.gr@dartmouth.edu>
2025-02-22 22:58:34 -0500

    check card and dealer

--- rlcard/games/mahjong/dealer.py
+++ rlcard/games/mahjong/dealer.py
@@ -1,9 +1,13 @@
+import numpy as np
+
+from rlcard.games.mahjong.player import MahjongPlayer
 from rlcard.games.mahjong.utils import init_deck
 
 
 class MahjongDealer:
     ''' Initialize a mahjong dealer class
     '''
+
     def __init__(self, np_random):
         self.np_random = np_random
         self.deck = init_deck()
@@ -19,16 +23,25 @@ class MahjongDealer:
         ''' Deal some cards from deck to one player
 
         Args:
-            player (object): The object of DoudizhuPlayer
+            player (object): The object of MahjongPlayer
             num (int): The number of cards to be dealed
         '''
         for _ in range(num):
             player.hand.append(self.deck.pop())
 
 
-## For test
-#if __name__ == '__main__':
-#    dealer = MahjongDealer()
-#    for card in dealer.deck:
-#        print(card.get_str())
-#    print(len(dealer.deck))
+# # For test
+# if __name__ == '__main__':
+#
+#     dealer = MahjongDealer(np.random.RandomState())
+#     print(len(dealer.deck))
+#
+#     player = MahjongPlayer(1, np.random.RandomState())
+#
+#     # Deal 3 cards to the player
+#     dealer.deal_cards(player, 3)
+#     print("!")
+#
+#     # Print out the player's hand and remaining deck
+#     for i in player.hand:
+#         print("Player's hand:", i.get_str())
